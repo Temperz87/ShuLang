804bb7de53dbce183edcb5724cc6d9bb8cae8af2
Shulin Gonsalves <shugons@iu.edu>
2025-04-13 14:30:10 -0400

    Make parser files, they're currently empty but shhh

---
+++ src/include/parser.hpp
@@ -0,0 +1,8 @@
+#include <ctype.h>
+#include <iostream>
+#include <string.h>
+#include "tokenizer.hpp"
+#include <vector>
+
+// TODO: DOCUMENTATION
+void parse(std::vector<token> tokenlist);
---
+++ src/parser/parser.cpp
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include <iostream>
+#include <string.h>
+#include <tokenizer.hpp>
+#include <vector>
+
+// Handwritten recursive descent parser
+
+
+
+void parse(std::vector<token> tokenlist) {
+    
+}
