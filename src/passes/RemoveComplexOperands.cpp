0e3ad53aba194c1782828c34f26461e930849cf1
Temperz87 <= temperz871@gmail.com>
2025-06-10 00:54:53 -0400

    Comments :O

--- src/passes/RemoveComplexOperands.cpp
+++ src/passes/RemoveComplexOperands.cpp
@@ -1,18 +1,16 @@
-#include "ASTPrinter.hpp"
 #include <AST.hpp>
 #include <ASTVisitor.hpp>
 #include <RemoveComplexOperands.hpp>
-#include <iterator>
 #include <string>
 #include <vector>
 
 static int tmp_counter = 0;
 
-
 std::string gen_tmp_name() {
     return "tmp" + std::to_string(tmp_counter++);
 }
 
+// Anything this class walks will be converted to be atomic
 class Atomify : public ASTVisitor {
     public:
         std::vector<BindingNode*>& bindings; 
@@ -33,15 +31,23 @@ class Atomify : public ASTVisitor {
         }
 };
 
+// This class takes in an AST that doesn't have to be atomic
+// And tries to ensure its children are
 class target_complex : public ASTVisitor {
     public:
         std::vector<BindingNode*> bindings;
         
         ASTNode* egressOperatorApplicationNode(OperatorApplicationNode* node) override {
+            // The bindings in the class clearly get propagated through every egress
+            // So we must have unique bindings for every time we Atomify
             std::vector<BindingNode*> bindings;
 
+            // For operators, their lhs and rhs must be atomic
             Atomify a = Atomify(bindings);
             a.walk(node->lhs);
+
+            // If there are new bindings then lhs wasn't atomic
+            // So reference the last binding as the new lhs
             if (!bindings.empty()) {
                 BindingNode* last_node = bindings.at(bindings.size() - 1);
                 VariableReferenceNode* new_value = new VariableReferenceNode(last_node->name);
@@ -50,18 +56,24 @@ class target_complex : public ASTVisitor {
             long size = bindings.size();
 
             a.walk(node->rhs);
+            
+            // If the size changed then there are new bindings
             if (bindings.size() != size) {
                 BindingNode* last_node = bindings.at(bindings.size() - 1);
                 VariableReferenceNode* new_value = new VariableReferenceNode(last_node->name);
                 node->rhs = new_value;
             }
 
+            // Insert to this->bindings
+            // Which kind of acts as a return
+            // Could probably change this to like an iterator position to update
             this->bindings.insert(this->bindings.end(), bindings.begin(), bindings.end());
             return ASTVisitor::egressOperatorApplicationNode(node);
         }
 
         ASTNode* egressPrintNode(PrintNode* node) override {
             Atomify a = Atomify(bindings);
+            // to_print must be atomic
             a.walk(node->to_print);
             if (!bindings.empty()) {
                 BindingNode* last_node = bindings.at(bindings.size() - 1);
