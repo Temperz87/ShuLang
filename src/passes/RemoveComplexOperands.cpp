9f376b268b34a00739b2bef9c6acef8dc0806a93
Temperz87 <= temperz871@gmail.com>
2025-08-04 11:14:00 -0400

    fix: Remove complex operands assigning bindings in backwards order

--- src/passes/RemoveComplexOperands.cpp
+++ src/passes/RemoveComplexOperands.cpp
@@ -11,6 +11,10 @@
 // And tries to ensure its children are
 class target_complex : public ShuLangVisitor {
     private:
+        std::vector<std::shared_ptr<ShuLangNode>>* program_nodes;
+        bool updated_inserted = true;
+        long insert_position;
+
         std::shared_ptr<VariableReferenceNode> generate_binding(std::shared_ptr<ValueNode> complex_value) {
             static int tmp_counter = 0;
             std::string name = "rco_output." + std::to_string(tmp_counter++);
@@ -20,12 +24,19 @@ class target_complex : public ShuLangVisitor {
             // TODO: Assign type
             fresh->ty = complex_value->type;
             fresh->value = complex_value;
-            bindings.push_back(fresh);
+            program_nodes->insert(program_nodes->begin() + insert_position, fresh);
+            insert_position += 1;
+
+            if (updated_inserted) {
+                inserted += 1;
+            }
+
             return std::make_unique<VariableReferenceNode>(name);
         }
 
     public:
-        std::vector<std::shared_ptr<BindingNode>> bindings;
+        int inserted = 0;
+        target_complex(std::vector<std::shared_ptr<ShuLangNode>>* nodes, long current_position):program_nodes(nodes), insert_position(current_position) { }
         
         ShuLangNode* egressOperatorApplicationNode(OperatorApplicationNode* node) override {
             // For operators, their lhs and rhs must be atomic
@@ -41,7 +52,6 @@ class target_complex : public ShuLangVisitor {
         }
 
         ShuLangNode* egressPrintNode(PrintNode* node) override {
-            ComplexDetector det;
             // to_print must be atomic
             if (ComplexDetector::IsComplex(node->to_print.get())) {
                 node->to_print = generate_binding(node->to_print);
@@ -50,7 +60,6 @@ class target_complex : public ShuLangVisitor {
         }
 
         ShuLangNode* egressIfNode(IfNode* node) override {
-            ComplexDetector det;
             // Condition must be atomic
             if (ComplexDetector::IsComplex(node->condition.get())) {
                 node->condition = generate_binding(node->condition);
@@ -61,17 +70,19 @@ class target_complex : public ShuLangVisitor {
             // as well as the condition :3
             return ShuLangVisitor::egressIfNode(node);
         }
+
+        ASTHolder ingressBodyNode(BodyNode* node, int childcount) override {
+            program_nodes = &node->nodes;
+            insert_position = 0;
+            updated_inserted = false;
+            return ShuLangVisitor::ingressBodyNode(node, childcount);
+        }
 };
 
 void remove_complex_operands(ProgramNode* program) {
     for (long i = 0; i < program->nodes.size(); i++) {
-        target_complex visitor = target_complex();
+        target_complex visitor = target_complex(&program->nodes, i);
         visitor.walk(program->nodes.at(i).get());
-
-        int len = visitor.bindings.size();
-        for (int j = 0; j < len; j++) {
-            program->nodes.insert(program->nodes.begin() + i, std::move(visitor.bindings.at(j)));
-            i += 1;
-        }
+        i += visitor.inserted;
     }
 }
