2427d7c79f4e0261303bbfd1fea5ec07471f0ff0
Temperz87 <temperz871@gmail.com>
2025-08-20 06:50:01 -0400

    Cleanup comments in ASTNode.hpp

--- src/include/ASTNode.hpp
+++ src/include/ASTNode.hpp
@@ -4,10 +4,4 @@ class ASTNode {
     public:
         ASTNode() { }
         ~ASTNode() { }
-        
-        // // Gets DIRECT children
-        // virtual std::vector<ASTNode*> children() = 0;
-
-        // virtual childholder<ASTNode> ingressVisitor (Visitor* visitor) = 0;
-        // virtual ASTNode* egressVisitor(Visitor* visitor) = 0;
 };
